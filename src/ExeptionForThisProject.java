c664c9786e46d9416d53b118247de9056da4c5f2
andrey.dorovskikh <a.dorovskih2012@yandex.ru>
2018-10-03 00:16:25 +0300

    Разобрался с Exception

--- src/ExeptionForThisProject.java
+++ src/ExeptionForThisProject.java
@@ -2,7 +2,6 @@ public class ExeptionForThisProject extends NumberFormatException {
 
 
     public ExeptionForThisProject(String message) {
-        //TODO: Как мне вывести этот Exeption?
         System.out.println("Нам очень жаль, но здесь закралась какая то ошибка..");
     }
 
